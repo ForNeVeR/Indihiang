b7640411dc3e47e1a1c5e2a38cc2a0974354abc6
SND\agusk_cp <SND\agusk_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-06-27 22:02:25 +0000

    git-svn-id: https://indihiang.svn.codeplex.com/svn@22505 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- Indihiangv1.0/Indihiang/Indihiang/Cores/Features/RequestFeature.cs
+++ Indihiangv1.0/Indihiang/Indihiang/Cores/Features/RequestFeature.cs
@@ -13,7 +13,7 @@ namespace Indihiang.Cores.Features
             _featureName = LogFeature.REQUEST;
 
             LogCollection log = new LogCollection();
-            _logs.Add("General", log);
+            _logs.Add("TimeTaken", log);            
         }
 
         protected override bool RunFeature(List<string> header, string[] item)
@@ -34,12 +34,59 @@ namespace Indihiang.Cores.Features
 
         private void RunW3cext(List<string> header, string[] item)
         {
+            int val = 0;
+            int index = header.IndexOf("date");
+            int index2 = header.IndexOf("time");
+            int index3 = header.IndexOf("cs-uri-stem");
+            int index4 = header.IndexOf("time-taken");
+
+            if (index == -1 || index2==-1)
+                return;
+
+            string key1 = item[index];
+            string key2 = item[index2];
+            string key = string.Format("{0} {1}", key1, key2);
+
+            string data1;
+            if (index3 > 0)
+                data1 = item[index3];
+            else
+                data1 = string.Empty;
+            string data2;
+            if (index4 > 0)
+                data2 = item[index4];
+            else
+                data2 = string.Empty;
+
+            if (!string.IsNullOrEmpty(data1) && data1 != "-" && !string.IsNullOrEmpty(data2) && data2 != "-")
+            {
+                _logs["TimeTaken"].Colls.Add(key, new WebLog(data1, data2));
+            }
+
         }
 
         protected override bool RunSynchFeatureData(Dictionary<string, LogCollection> newItem)
         {
             bool success = false;
-
+            try
+            {
+                foreach (KeyValuePair<string, LogCollection> pair in newItem)
+                {
+                    if (pair.Key == "TimeTaken")
+                    {
+                        foreach (KeyValuePair<string, WebLog> pair2 in pair.Value.Colls)
+                        {
+                            if (!_logs[pair.Key].Colls.ContainsKey(pair2.Key))
+                                _logs[pair.Key].Colls.Add(pair2.Key, pair2.Value);
+                        }
+                    }
+                }
+                success = true;
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(String.Format("Error Synch: {0}", err.Message));
+            }
             return success;
         }
     }
---
+++ Indihiangv1.0/Indihiang/Indihiang/Modules/BandwidthControl.Designer.cs
@@ -0,0 +1,45 @@
+namespace Indihiang.Modules
+{
+    partial class BandwidthControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BandwidthControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "BandwidthControl";
+            this.Size = new System.Drawing.Size(709, 434);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
