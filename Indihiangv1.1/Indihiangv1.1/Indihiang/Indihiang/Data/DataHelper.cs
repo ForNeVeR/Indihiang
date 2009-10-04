0a15a2f1162954cc83e5b7686f8c90bd88fc9d72
SND\agusk_cp <SND\agusk_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2009-10-04 11:46:14 +0000

    git-svn-id: https://indihiang.svn.codeplex.com/svn@29254 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- Indihiangv1.1/Indihiangv1.1/Indihiang/Indihiang/Data/DataHelper.cs
+++ Indihiangv1.1/Indihiangv1.1/Indihiang/Indihiang/Data/DataHelper.cs
@@ -19,161 +19,172 @@ namespace Indihiang.Data
         {
             return string.Format("Data Source={0}",_dbFile);
         }
-        public void InsertFeature(string name,string field)
-        {
-            SQLiteConnection con = null;
 
-            try
-            {
-                con = new SQLiteConnection(GetConnectionStrng());
-                con.Open();
+        #region Temp
+        //public void InsertFeature(string name,string field)
+        //{
+        //    SQLiteConnection con = null;
 
-                string query = String.Format("insert into feature(name,field) values('{0}','{1}')", name,field);
-                using (SQLiteCommand cmd = new SQLiteCommand(query, con))
-                {
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                if (con != null)
-                    con.Close();
-            }
-        }
-        public void InsertShared(string name, string val)
-        {
-            SQLiteConnection con = null;
+        //    try
+        //    {
+        //        con = new SQLiteConnection(GetConnectionStrng());
+        //        con.Open();
 
-            try
-            {
-                con = new SQLiteConnection(GetConnectionStrng());
-                con.Open();
+        //        string query = String.Format("insert into feature(name,field) values('{0}','{1}')", name,field);
+        //        using (SQLiteCommand cmd = new SQLiteCommand(query, con))
+        //        {
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //    finally
+        //    {
+        //        if (con != null)
+        //            con.Close();
+        //    }
+        //}
+        //public void InsertShared(string name, string val)
+        //{
+        //    SQLiteConnection con = null;
 
-                string query = String.Format("insert into shared(name,val) values('{0}','{1}')", name, val);
-                using (SQLiteCommand cmd = new SQLiteCommand(query, con))
-                {
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                if (con != null)
-                    con.Close();
-            }
-        }
-        public void InsertFeatureData(string featureName, string field, string data1, string data2, string data3)
-        {
-            SQLiteConnection con = null;
-            SQLiteDataReader rd = null;
-            try
-            {
-                con = new SQLiteConnection(GetConnectionStrng());
-                con.Open();
+        //    try
+        //    {
+        //        con = new SQLiteConnection(GetConnectionStrng());
+        //        con.Open();
 
-                int id = -1;
-                string query = String.Format("select id from feature where name like '{0}' and field like '{1}'", featureName, field);
-                SQLiteCommand cmd = new SQLiteCommand(query, con);
-                rd = cmd.ExecuteReader();
-                if (rd.Read())
-                {
-                    if (!rd.IsDBNull(rd.GetOrdinal("id")))
-                        id = (int)rd["id"];
-                }
-                rd.Close();
+        //        string query = String.Format("insert into shared(name,val) values('{0}','{1}')", name, val);
+        //        using (SQLiteCommand cmd = new SQLiteCommand(query, con))
+        //        {
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //    finally
+        //    {
+        //        if (con != null)
+        //            con.Close();
+        //    }
+        //}
+        //public void InsertFeatureData(string featureName, string field, string data1, string data2, string data3)
+        //{
+        //    SQLiteConnection con = null;
+        //    SQLiteDataReader rd = null;
+        //    try
+        //    {
+        //        con = new SQLiteConnection(GetConnectionStrng());
+        //        con.Open();
 
-                if (id > 0)
-                {
-                    query = String.Format("insert into featuredata(featureid,val1,val2,val3) values({0},'{1}','{2}','{3}')", id, data1, data2, data3);
-                    cmd = new SQLiteCommand(query, con);
-                    cmd.ExecuteNonQuery();
-                }
+        //        int id = -1;
+        //        string query = String.Format("select id from feature where name like '{0}' and field like '{1}'", featureName, field);
+        //        SQLiteCommand cmd = new SQLiteCommand(query, con);
+        //        rd = cmd.ExecuteReader();
+        //        if (rd.Read())
+        //        {
+        //            if (!rd.IsDBNull(rd.GetOrdinal("id")))
+        //                id = (int)rd["id"];
+        //        }
+        //        rd.Close();
+
+        //        if (id > 0)
+        //        {
+        //            query = String.Format("insert into featuredata(featureid,val1,val2,val3) values({0},'{1}','{2}','{3}')", id, data1, data2, data3);
+        //            cmd = new SQLiteCommand(query, con);
+        //            cmd.ExecuteNonQuery();
+        //        }
                 
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                if (rd != null)
-                    rd.Close();
-                if (con != null)
-                    con.Close();
-            }
-        }
-        public int GetSharedId(string name,string val)
-        {
-            int id = -1;
-            SQLiteConnection con = null;
-            SQLiteDataReader rd = null;
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //    finally
+        //    {
+        //        if (rd != null)
+        //            rd.Close();
+        //        if (con != null)
+        //            con.Close();
+        //    }
+        //}
+        //public int GetSharedId(string name,string val)
+        //{
+        //    int id = -1;
+        //    SQLiteConnection con = null;
+        //    SQLiteDataReader rd = null;
 
-            try
-            {
-                con = new SQLiteConnection(GetConnectionStrng());
-                con.Open();
+        //    try
+        //    {
+        //        con = new SQLiteConnection(GetConnectionStrng());
+        //        con.Open();
 
-                string query = String.Format("select id from shared where name like '{0}' and val like '{1}'", name, val);
-                using (SQLiteCommand cmd = new SQLiteCommand(query, con))
-                {
-                    rd = cmd.ExecuteReader();
-                    if (rd.Read())
-                        if (!rd.IsDBNull(rd.GetOrdinal("id")))
-                            id = (int)rd["id"];
-                }
+        //        string query = String.Format("select id from shared where name like '{0}' and val like '{1}'", name, val);
+        //        using (SQLiteCommand cmd = new SQLiteCommand(query, con))
+        //        {
+        //            rd = cmd.ExecuteReader();
+        //            if (rd.Read())
+        //                if (!rd.IsDBNull(rd.GetOrdinal("id")))
+        //                    id = (int)rd["id"];
+        //        }
 
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                if (rd != null)
-                    rd.Close();
-                if (con != null)
-                    con.Close();
-            }
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //    finally
+        //    {
+        //        if (rd != null)
+        //            rd.Close();
+        //        if (con != null)
+        //            con.Close();
+        //    }
 
-            return id;
-        }
-        public int GetFeaturedDataId(string featureName, int shareId,string val1)
-        {
-            int id = -1;
-            SQLiteConnection con = null;
-            SQLiteDataReader rd = null;
+        //    return id;
+        //}
+        //public int GetFeaturedDataId(string featureName, int shareId,string val1)
+        //{
+        //    int id = -1;
+        //    SQLiteConnection con = null;
+        //    SQLiteDataReader rd = null;
 
-            try
-            {
-                con = new SQLiteConnection(GetConnectionStrng());
-                con.Open();
+        //    try
+        //    {
+        //        con = new SQLiteConnection(GetConnectionStrng());
+        //        con.Open();
 
-                string query = String.Format("select id from featuredata where featurename like '{0}' and shareid={1} and val1 like '{2}'", featureName, shareId, val1);
-                SQLiteCommand cmd = new SQLiteCommand(query, con);
-                rd = cmd.ExecuteReader();
-                if (rd.Read())
-                {
-                    if (!rd.IsDBNull(rd.GetOrdinal("id")))
-                        id = (int)rd["id"];
-                }
+        //        string query = String.Format("select id from featuredata where featurename like '{0}' and shareid={1} and val1 like '{2}'", featureName, shareId, val1);
+        //        SQLiteCommand cmd = new SQLiteCommand(query, con);
+        //        rd = cmd.ExecuteReader();
+        //        if (rd.Read())
+        //        {
+        //            if (!rd.IsDBNull(rd.GetOrdinal("id")))
+        //                id = (int)rd["id"];
+        //        }
 
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                if (rd != null)
-                    rd.Close();
-                if (con != null)
-                    con.Close();
-            }
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //    finally
+        //    {
+        //        if (rd != null)
+        //            rd.Close();
+        //        if (con != null)
+        //            con.Close();
+        //    }
 
-            return id;
-        }
-        
+        //    return id;
+        //}
+
+        #endregion
+
+
+        #region Indihiang
+        //public void InsertIndihiang()
+
+
+        #endregion
+
     }
 }
